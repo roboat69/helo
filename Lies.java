9395bca1e993ec91d054629a77c98a3c9b71dc6a
Rohit Seshadri <rohitlemonheads@gmail.com>
2021-06-11 14:25:59 -0700

    nerd xxdxxdxrddxdxxfxfxdxxxxdxdxd

--- Lies.java
+++ Lies.java
@@ -1,5 +1,5 @@
 public class Lies {
     public static void main(String[] args) {
-        System.out.println("git is awesome !! ! ! ! !  ! ! !!   ! ! !!  ! ! !! !  ! !");
+        System.out.println("git is awesome !! ! ! ! !  dfdfdfdfdfdfdfdfdfdfdf! ! !!   ! ! !!  ! ! !! !  ! !");
     }
 }
